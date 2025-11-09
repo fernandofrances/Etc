1ff43da90f7ba3e1a6c05b86d9550820872e7215
Fernando Frances <fernandofrances@Fernandos-MacBook-Pro.local>
2025-11-09 19:50:43 +0100

    initial commit

---
+++ Etc/EtcApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtcApp.swift
+//  Etc
+//
+//  Created by Fernando Frances on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct EtcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
